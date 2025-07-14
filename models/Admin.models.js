d9d988ba946ded3857bfe73b99c59d1bd145c8b5
arpan-gt <arpan.main@outlook.com>
2025-07-14 12:11:12 +0530

    admin model created

---
+++ models/Admin.models.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const adminSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      minlength: 2,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+adminSchema.methods.comparePassword = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
+};
+
+const Admin = mongoose.model("Admin", adminSchema);
+export { Admin };
