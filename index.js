7af232021b1c62a27dca30e54a9ddf9597fd6206
arpan-gt <arpan.main@outlook.com>
2025-07-14 11:43:56 +0530

    express server setup

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
